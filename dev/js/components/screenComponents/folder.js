8b64dfb32d675dc48f3afa59b74af4f4cdddfe3e
lukaszwachowiak <lukasz.wachowiak@op.pl>
2019-07-20 14:10:16 +0200

    folder open/close no problemo

--- dev/js/components/screenComponents/folder.js
+++ dev/js/components/screenComponents/folder.js
@@ -1,48 +1,16 @@
 import React, {Component} from "react";
 import {Fragment} from "react";
 
-class Folder extends Component {
-    constructor(props){
-        super(props);
-        this.state={
-            close: false,
-        }
-    }
-
-    click(){
-        this.setState({
-            close: true,
-        })
-    }
-
-    render(){
-        const folder = (
-            <div>
-                <header>
-                    <div onClick={e => this.click(e)}>+</div>
-                    {/*^ wyłączanie folderu, krzyżyk, obrócić go z pomocą transform: rotate(45deg)*/}
-                </header>
-                <nav>
-                    <ul>
-                        {/*wczytywanie elementów li .map*/}
-                    </ul>
-                </nav>
-            </div>
-        );
-
-        return this.state.close ? null : folder;
-    }
-}
-
 export default class FolderIcon extends Component {
     constructor(props){
         super(props);
         this.state={
             clicked: false,
+            close: false,
         }
     }
 
-    clicked(e){
+    open(e){
         e.preventDefault();
 
         this.setState({
@@ -50,19 +18,102 @@ export default class FolderIcon extends Component {
         })
     }
 
+    close(){
+        this.setState({
+            close: true,
+            clicked: false,
+        })
+    }
+
     render(){
+        const folder = (
+            <div>
+                <header>
+                    <div onClick={e => this.close(e)}>+</div>
+                    {/*^ wyłączanie folderu, krzyżyk, obrócić go z pomocą transform: rotate(45deg)*/}
+                </header>
+                <aside>
+                    <ul>
+                        {/*wczytywanie elementów li .map*/}
+                    </ul>
+                </aside>
+            </div>
+        );
+
         return (
             <Fragment>
-                <div
-                    onDoubleClick={e => this.clicked(e)}
-                >
+                <div onDoubleClick={e => this.open(e)}>
                     <div></div>
                     {/*z pomocą :before i :after stworzyć dwa pseudoelementy*/}
                     {/*oraz z pomocą transform: skewX(25deg); zrobić z nich dwa romby powyżej powyższego diva*/}
                     <p>Pliki</p>
                 </div>
-                {this.state.clicked ? <Folder/> : null}
+                {this.state.clicked ? folder : null}
             </Fragment>
         );
     }
 }
+
+// class Folder extends Component {
+//     constructor(props){
+//         super(props);
+//         this.state={
+//             close: false,
+//         }
+//     }
+//
+//     close(){
+//         this.setState({
+//             close: true,
+//         })
+//     }
+//
+//     render(){
+//         const folder = (
+//             <div>
+//                 <header>
+//                     <div onClick={e => this.close(e)}>+</div>
+//                     {/*^ wyłączanie folderu, krzyżyk, obrócić go z pomocą transform: rotate(45deg)*/}
+//                 </header>
+//                 <aside>
+//                     <ul>
+//                         {/*wczytywanie elementów li .map*/}
+//                     </ul>
+//                 </aside>
+//             </div>
+//         );
+//
+//         return this.state.close ? null : folder;
+//     }
+// }
+//
+// export default class FolderIcon extends Component {
+//     constructor(props){
+//         super(props);
+//         this.state={
+//             clicked: false,
+//         }
+//     }
+//
+//     clicked(e){
+//         e.preventDefault();
+//
+//         this.setState({
+//             clicked: true,
+//         })
+//     }
+//
+//     render(){
+//         return (
+//             <Fragment>
+//                 <div onDoubleClick={e => this.clicked(e)}>
+//                     <div></div>
+//                     {/*z pomocą :before i :after stworzyć dwa pseudoelementy*/}
+//                     {/*oraz z pomocą transform: skewX(25deg); zrobić z nich dwa romby powyżej powyższego diva*/}
+//                     <p>Pliki</p>
+//                 </div>
+//                 {this.state.clicked ? <Folder/> : null}
+//             </Fragment>
+//         );
+//     }
+// }
